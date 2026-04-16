8610599ab0ef9d602609db5c4c03a1dff9b04dda
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-04-16 15:04:56 +0100

    chore(app): add IsCustomer middleware to middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
         'manager' => \App\Http\Middleware\IsManager::class,
+        'customer' => \App\Http\Middleware\IsCustomer::class,
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
